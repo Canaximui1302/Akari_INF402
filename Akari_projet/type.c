25c0c8463b311488b170fd714a9f860c6c4c8e21
Gabriel <120597060+precupgabrielantoniu@users.noreply.github.com>
2024-04-25 23:49:19 +0200

    Update type.c with setter, getter and print table

--- Akari_projet/type.c
+++ Akari_projet/type.c
@@ -3,30 +3,75 @@
 #include "solveur.h"
 #include "type.h"
 
-Tableau init_jeu(int taille)
+Tableau tableau_vide(int taille)
 {
+    if(taille > TAILLE)
+    {
+        printf("Taille trop grande.\n");
+        return tableau_vide(0);
+    }
     Tableau t;
     t.taille = taille;
-    
+    for(int i = 0; i < taille; i++)
+    {
+        for(int j = 0; j < taille; j++)
+        {
+            t.tab[i][j] = V;
+        }
+    }
     return t;
 
 }
 
-void mettre_ampoule(Tableau *t, int x, int y)
+void affichage_tableau(Tableau t)
 {
-    Cell c = t->tab[y][x];
-    switch(c)
+    for(int i = 0; i < t.taille; i++)
     {
-        case A:
-            break;
-        case B:
-            t->tab[y][x] = A;
-            break;
-        default:
-            printf("Coup invalide.\n");
-            break;
+        printf("--");
+    }
+    printf("-\n");
+    for(int i = 0; i < t.taille; i++)
+    {
+        for(int j = 0; j < t.taille; j++)
+        {
+            printf("|");
+            if(t.tab[i][j] == N) printf("n");
+            else if(t.tab[i][j] == N0 || 
+            t.tab[i][j] == N1 || 
+            t.tab[i][j] == N2 ||
+            t.tab[i][j] == N3 ||
+            t.tab[i][j] == N4) printf("%d", t.tab[i][j]-1);
+            else if(t.tab[i][j] == A) printf("*");
+            else if(t.tab[i][j] == B) printf(" ");
+            else printf("?");
+
+        }
+        printf("|\n");
+        for(int k = 0; k < t.taille; k++)
+        {
+            printf("--");
+        }
+        printf("-\n");
     }
 }
 
+void set_valeur(Tableau *t, int ln, int cl, Cell c)
+{
+    Cell c_precedente = t->tab[ln][cl];
+    if(c == c_precedente) return;
+    switch(c_precedente)
+    {
+        case V:
+            t->tab[ln][cl] = c;
+            break;
+        default:
+            printf("Coup invalide.\n");
+    }
+}
 
+Cell get_valeur(Tableau t, int ln, int cl)
+{
+    if(ln < 0 || ln >= t.taille || cl < 0 || cl >= t.taille) return N;
+    return t.tab[ln][cl];
+}
 
